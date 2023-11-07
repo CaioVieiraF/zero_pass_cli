51758f294d83834af41a463f7af79c39ae7de01a
Caio Fernandes <caiovieiraprofissional@proton.me>
2023-11-07 10:34:08 -0300

    Remove the need of the configuration file

--- src/languages.rs
+++ src/languages.rs
@@ -1,10 +1,21 @@
 use crate::prelude::*;
+use std::env;
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub enum Languages {
     PtBr,
     #[default]
     EnUs,
+}
+
+impl From<String> for Languages {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "pt_BR.UTF-8" => Languages::PtBr,
+            "en_US.UTF-8" => Languages::EnUs,
+            _ => Languages::default(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -27,16 +38,26 @@ pub struct Messages<'a> {
     pub final_result: &'a str,
 }
 
+impl<'a> Default for Messages<'a> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl<'a> Messages<'a> {
-    pub fn new(lang: Languages) -> Self {
+    pub fn new() -> Self {
+        let lang = match env::var("LANG") {
+            Ok(l) => Languages::from(l),
+            Err(_) => Languages::default(),
+        };
         match lang {
             Languages::PtBr => Messages {
                 ask_menu_method: "Escolha um método de criptografia",
                 ask_unique_pass: "Digite a senha única",
                 ask_variable_pass: "Digite a senha variável",
-                ask_get_sys_default_method: "Usar o método padrão do sistema?[s/n] ",
+                ask_get_sys_default_method: "Usar o método padrão do sistema? ",
                 ask_repeat_method_times: "Número de repetições: ",
-                ask_create_file: "Você não tem um arquivo de configuração, quer criar um?[s/n] ",
+                ask_create_file: "Você não tem um arquivo de configuração, quer criar um? ",
                 error_input: "Falha ao ler a entrada!",
                 error_invalid_character: "O caractere inserido é inválido",
                 error_parse: "Erro: O valor inserido tem que ser um número",
@@ -53,9 +74,9 @@ impl<'a> Messages<'a> {
                 ask_menu_method: "Choose a cryptography method",
                 ask_unique_pass: "Type your unique password",
                 ask_variable_pass: "Type the variable password",
-                ask_get_sys_default_method: "Use the system's default method?[y/n] ",
+                ask_get_sys_default_method: "Use the system's default method? ",
                 ask_repeat_method_times: "Number of repetitions: ",
-                ask_create_file: "You don't have a configuration file, want to create one?[y/n] ",
+                ask_create_file: "You don't have a configuration file, want to create one? ",
                 error_input: "Failed to read input",
                 error_invalid_character: "The character is invalid",
                 error_parse: "Erro: the value must be a number",
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@ pub mod prelude;
 use std::str::FromStr;
 
 use clap::Parser;
-use inquire::{Confirm, Password, Select, Text};
-use languages::{Languages, Messages};
-use toml::Value;
+use inquire::{Password, Select, Text};
+use languages::Messages;
 use zero_pass_backend::{self as zpb, encrypt::PasswordBuilder};
 use zpb::Methods;
 
@@ -29,30 +28,8 @@ struct Args {
 }
 
 fn main() {
-    // Define the configuration file and the language variables.
-    let mut config_file: Option<Value> = None;
-    let lang: Languages;
-
-    // Tryies to load the file. If loaded successfully it sets the config_file variable and tryies
-    // to get the defined language if any. If the file is not loaded, then we use the default
-    // language.
-    match load_file(Messages::new(Languages::EnUs)) {
-        Some(f) => {
-            config_file = Some(f);
-            lang = config_file.clone().unwrap()["props"]["lang"]
-                .as_str()
-                .unwrap()
-                .parse::<Languages>()
-                .unwrap();
-        }
-        None => {
-            println!("Could not load configuration file, fallbacking to en_us...");
-            lang = Languages::default()
-        }
-    }
-
-    // Instantiate the messages helper.
-    let mess = Messages::new(lang);
+    // Instantiate the messages helper that sets the language.
+    let mess = Messages::new();
 
     // Get the command arguments from the environment.
     let cli_args = Args::parse();
@@ -80,56 +57,15 @@ fn main() {
     // Get method from command line argument or prompt
     let method = match cli_args.method {
         Some(m) => m.to_method(),
-        None => match config_file {
-            Some(f) => {
-                let method_name = f["props"]["default_method"]
-                    .as_str()
-                    .expect(mess.error_file_parse);
-                let method = Methods::try_from(method_name).expect(mess.error_file_prop);
-                method.to_method()
-            }
-            None => {
-                let choice = Select::new(mess.ask_menu_method, Methods::get_methods()).prompt();
-                let method = Methods::from_str(choice.unwrap()).expect(mess.error_unknown_method);
-                method.to_method()
-            }
-        },
+        None => {
+            let choice = Select::new(mess.ask_menu_method, Methods::get_methods()).prompt();
+            let method = Methods::from_str(choice.unwrap()).expect(mess.error_unknown_method);
+            method.to_method()
+        }
     };
     password_builder = password_builder.method_ptr(method).unwrap();
     // Get the generated password and then show to the user.
     let result: String = password_builder.build();
 
     println!("{} \"{result}\"", mess.final_result);
-}
-
-fn load_file(mess: Messages) -> Option<Value> {
-    use std::fs::File;
-    use std::io::{Read, Write};
-
-    let mut home = dirs::home_dir()?;
-
-    home.push(".config/zero_pass/config.toml");
-    let file_path = home;
-    let file = match File::open(&file_path).ok() {
-        Some(file) => Some(file),
-        None => match Confirm::new(mess.ask_create_file)
-            .with_default(false)
-            .prompt()
-        {
-            Ok(true) => {
-                let mut file = File::create(&file_path).expect("Could not create file!");
-                file.write_all(b"[props]\ndefault_method = 'Base64'\nlang = 'EnUs'")
-                    .expect("Could not write to file!");
-                Some(file)
-            }
-            _ => None,
-        },
-    };
-    let mut s = String::new();
-    file?.read_to_string(&mut s).expect("Não foi possível ler");
-
-    Some(
-        s.parse::<Value>()
-            .expect("Erro ao ler o arquivo no formato TOML."),
-    )
 }
